766a2c42468b297e399f3c5715b4a35e7498eb4b
Jakob Kielland <jakob-ek@hotmail.com>
2022-03-29 22:23:42 +0200

    add monitor bezel unless mobile breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
   return (
     // Style this div to effectively change the body style
     <div className='h-screen overflow-auto'>
-      <Routes>
-        <Route path='/' element={<HomeView />} />
-      </Routes>
+      <div className='w-full h-full md:monitor-outer-bezel'>
+        <div className='w-full h-full md:monitor-inner-bezel'>
+          <div className='relative w-full h-full md:monitor-screen monitor-screen-mobile'>
+            <Routes>
+              <Route path='/' element={<HomeView />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/view/homeView.jsx
+++ src/view/homeView.jsx
@@ -3,7 +3,7 @@ import WindowsHeaderBar from '../components/windowsHeaderBar';
 const HomeView = () => {
   return (
     <>
-      <div className='flex flex-col h-full gap-1 win98-window'>
+      <div className='absolute flex flex-col w-5/6 gap-1 left-6 top-10 h-5/6 win98-window'>
         <WindowsHeaderBar windowName='JakobKielland.exe' />
         <div className='font-mono text-white bg-black win98-inner-border grow'>
           &gt; Hello world!

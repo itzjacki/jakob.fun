8110a29d0238d5c38b529f6694b47d4cd15c29e3
Jakob Kielland <jakob-ek@hotmail.com>
2023-05-26 01:58:17 +0200

    Add basic markdown support in cool_stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind({ config: { applyBaseStyles: false } })],
   output: "server",
   adapter: vercel(),
 });

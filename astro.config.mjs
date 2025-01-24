6bd0194f216f99cd367f1c500041a3423411854a
Jakob Kielland <jakob-ek@hotmail.com>
2025-01-24 15:17:31 +0100

    add rss feed to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config"
-import svelte from "@astrojs/svelte"
-import vercel from "@astrojs/vercel/serverless"
-import tailwind from "@astrojs/tailwind"
-import mdx from "@astrojs/mdx"
-import icon from "astro-icon"
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jakob.fun",
   integrations: [
     svelte(),
     tailwind({
@@ -20,6 +21,6 @@ export default defineConfig({
   output: "server",
   adapter: vercel(),
   redirects: {
-    '/modern-css': '/fun/modern-css'
-  }
-})
+    "/modern-css": "/fun/modern-css",
+  },
+});
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+  const blog = await getCollection("blog");
+  return rss({
+    title: "Jakob.fun",
+    description:
+      "The place where Jakob's silly and serious thoughts coalesce into words roughly biannually",
+    site: context.site,
+    items: blog
+      .filter((post) => !post.data.isDraft)
+      .map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.publishedDate,
+        description: post.data.excerpt,
+        link: `/blog/${post.slug}/`,
+      })),
+    customData: `<language>en-us</language>`,
+  });
+}

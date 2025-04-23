fa69d64ed6637f283209f9f04f04cd1590b9a197
Jakob Kielland <jakob-ek@hotmail.com>
2025-04-23 17:27:23 +0200

    make updates to css blogpost

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            "blockquote *::before": {
+              content: "none",
+            },
+            "blockquote *::after": {
+              content: "none",
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
-}
+};

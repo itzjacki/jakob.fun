b88181943dd673e01723c52cc4c61b90021dded0
Jakob Kielland <jakob-ek@hotmail.com>
2025-05-29 17:56:59 +0200

    upgrade tailwind version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jakob.fun",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    mdx(),
-    icon(),
-  ],
+  integrations: [mdx(), icon()],
   output: "server",
   adapter: vercel(),
+
   redirects: {
     "/modern-css": "/fun/modern-css",
     "/swiss-cheese": "/fun/swiss-cheese",
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
